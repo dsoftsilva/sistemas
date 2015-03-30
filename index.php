96bdaa805de50155874ead1599d251828120c8d8
dsoftsilva <dsoftsilva@hotmail.com>
2015-03-29 23:03:05 -0500

    comentario

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 
-	$con = mysql_connect($host,'root','asdas') or die('no se pudo conectar con el host');
+	$con = mysql_connect($host,'root','asdas') or die('no se pudo conectar con el host'); 
+	//conexion al servidor
 	mysql_select_db($db,$con) or die("problemas al conectar db");
+
  ?>
